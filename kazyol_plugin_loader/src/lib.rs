601d1c9a46d2c80694cf2e17bd2e0e5a301de521
Iaiao <err266228@gmail.com>
2021-01-10 20:13:39 +0200

    Plugin loader: Fixed skip empty lines

--- kazyol_plugin_loader/src/lib.rs
+++ kazyol_plugin_loader/src/lib.rs
@@ -5,7 +5,8 @@ pub fn load_plugins(_: TokenStream) -> TokenStream {
     let s = std::fs::read_to_string("plugins.txt").expect("Unable to read `plugins.txt`");
     let mut output = String::new();
     for plugin in s.lines() {
-        if s.trim().is_empty() {
+        if plugin.trim().is_empty() {
+            println!("empty");
             continue;
         }
         output += &format!("kazyol_lib::tracking::name(\"{}\".to_string());\n", plugin);
